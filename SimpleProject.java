a695b04459649c58033d4161b8f842ccb461bd29
KevinHa-coder <79331954+KevinHa-coder@users.noreply.github.com>
2021-03-19 11:05:52 -0700

    Updated version to 1.1

--- SimpleProject.java
+++ SimpleProject.java
@@ -1,6 +1,6 @@
 class SimpleProject{  
 
    public static void main(String[] args){  
-      System.out.println("This is java app \n by using Docker \n Version 1.0");  
+      System.out.println("This is java app \n by using Docker \n Version 1.1");  
    }    
 }  
